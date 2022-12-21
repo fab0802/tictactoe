f374bca8011411d020538bad0f5d6103060a1765
Fabian <fabian.gretsch@gmail.com>
2022-12-21 22:50:28 +0100

    added game functionality

--- script.js
+++ script.js
@@ -6,15 +6,25 @@ const playerCrossImg = document.querySelector("#player-cross-img");
 const playerCircleImg = document.querySelector("#player-circle-img");
 const playerCrossScore = document.querySelector("#core-player-cross");
 const playerCircleScore = document.querySelector("#core-player-circle");
+const boardBoxes = document.querySelectorAll(".game-board-box");
+const winningRows = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+let boxes = [];
 let isCrossNext = true;
 let twoPlayers = true;
 
 function setBox(box) {
-  if (isCrossNext) {
-    setCross(box);
-  } else {
-    setCircle(box);
-  }
+  if (boardBoxes[box].innerHTML === "")
+    isCrossNext ? setCross(box) : setCircle(box);
+  checkWinner(box);
 }
 
 function setCross(box) {
@@ -22,6 +32,7 @@ function setCross(box) {
   isCrossNext = false;
   playerCrossImg.classList.remove("next");
   playerCircleImg.classList.add("next");
+  boxes[box] = "cross";
 }
 
 function setCircle(box) {
@@ -29,4 +40,15 @@ function setCircle(box) {
   isCrossNext = true;
   playerCircleImg.classList.remove("next");
   playerCrossImg.classList.add("next");
+  boxes[box] = "circle";
+}
+
+function checkWinner(box) {
+  console.log(box);
+}
+
+function resetBoard() {
+  for (let box of boardBoxes) {
+    box.innerHTML = "";
+  }
 }
